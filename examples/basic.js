55c5adcc58639d6ff6d56f6649bd8203b127f461
Matthew Hatcher <hello@matthewh.in>
2016-04-25 19:03:16 -0400

    Fixing example.

--- examples/basic.js
+++ examples/basic.js
@@ -1,5 +1,7 @@
 // Coding standard for this project defined @ https://github.com/MatthewSH/standards/blob/master/JavaScript.md
 const $StreamMe   = require('../');
 var node          = new $StreamMe('node');
-console.log('node is ' + node.User.isLive() ? 'live' : 'not live');
+var isLive        = node.User.isLive() ? 'live' : 'not live';
+
+console.log('node is ' + isLive);
 console.log('node\'s profile has been viewed ' + node.Stats.getViews('human') + ' times.');
